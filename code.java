50a15dfdd774bb8aee6c46de319db65ce5ea98a3
Abdullah-Kz <romantech2@gmail.com>
2024-09-26 10:50:48 -0700

    .

--- code.java
+++ code.java
@@ -1,10 +1,11 @@
 
 import java.util.Scanner;
 public class code
-   {public static void main(String[] args) {
-
+{
+   public static void main(String[] args) {
    Scanner sc = new Scanner(System.in);
    int n = sc.nextInt();
    System.out.println("Hey " + n);
    sc.close();
-}}
+}
+}
